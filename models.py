f1ac8e6c60ef11436ac150285fdbdbb5c3a57a0c
Michael Arroyo <arroyo_michael@sbcglobal.net>
2017-02-14 13:43:24 -0600

    Fixing length for password hash in Users table Schema

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     firstname = db.Column(db.String(100))
     lastname = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
-    pwdhash = db.Column(db.String(54))
+    pwdhash = db.Column(db.String(100))
 
     def __init__(self, firstname, lastname, email, password):
         self.firstname = firstname.title()
--- routes.py
+++ routes.py
@@ -81,5 +81,4 @@ def login():
 
 
 if __name__ == "__main__":
-
     app.run(debug=True)
